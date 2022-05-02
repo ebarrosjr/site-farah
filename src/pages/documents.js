c0b3ce665b0ba3b6219157e7923d3e8af388ed1f
Victor <edvictorsr@gmail.com>
2022-05-02 17:05:54 -0400

    configuracoes iniciais e fotos -- alguns bugs

---
+++ src/pages/documents.js
@@ -0,0 +1,17 @@
+import Document,{Head, Html, Main, NextScript} from "next/document";
+
+export default class Mydocument extends Document{
+    render(){
+        return(
+            <Html>
+                <Head>
+            
+                </Head>
+                <body>
+                    <Main/>
+                    <NextScript/>
+                </body>
+            </Html>
+        )
+    }
+}
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import "../scss/styles.module.scss";
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>Centro Medico Dr. Fabio Farah</title>
+    </Head>
+    <div>
+      <h1>projeto com next</h1>
+    </div>
+    </>
+
+  )
+}
